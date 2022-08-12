10f9f50a630288569f9e82bc90b8179fbe423595
jquerin <jquerinucsd@gmail.com>
2022-08-11 23:32:10 -0700

    Modified model definitions to have unique tags where appplicable (#6)
    Modified model definitions to have unique tags where appplicable. Updated .env to use correct PORT format

--- server/pkg/common/models/user.go
+++ server/pkg/common/models/user.go
@@ -5,8 +5,8 @@ import "gorm.io/gorm"
 // User struct
 type User struct {
 	gorm.Model
-	Username string `json:"username" gorm:"unique_index;not null"`
-	Email    string `json:"email" gorm:"unique_index;not null"`
+	Username string `json:"username" gorm:"unique;not null"`
+	Email    string `json:"email" gorm:"unique;not null"`
 	Password string `json:"password" gorm:"not null"`
 	Names    string `json:"names"`
 }
