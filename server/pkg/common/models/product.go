322e90a9b49318751c1021b2d9e22990f48a0b4a
jquerin <jquerinucsd@gmail.com>
2022-08-11 23:30:50 -0700

    Modified model definitions to have unique tags where appplicable. Updated .env to use correct PORT format

--- server/pkg/common/models/product.go
+++ server/pkg/common/models/product.go
@@ -3,7 +3,7 @@ package models
 // Product is a model for a product
 type Product struct {
 	Id    int    `json:"id" gorm:"primaryKey"`
-	Name  string `json:"name" gorm:"unique_index;not null" example:"baseball"`
+	Name  string `json:"name" gorm:"unique;not null" example:"baseball"`
 	Stock int32  `json:"stock" gorm:"not null" example:"15"`
 	Price int32  `json:"price" gorm:"not null" example:"4"`
 }
